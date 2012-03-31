2d445f004de2692f4cb5acfba356d09bbe63012d
xxhp <xiao_hb@qq.com>
2012-03-31 23:37:14 +0800

    add files

---
+++ CustomTabBarController/SlidingTabsTab.m
@@ -0,0 +1,64 @@
+//
+//  SlidingTabsTab.m
+//  SlidingTabs
+//
+//  Created by Mathew Piccinato on 5/12/11.
+//  Copyright 2011 Constructt. All rights reserved.
+//
+
+#import "SlidingTabsTab.h"
+
+
+@implementation SlidingTabsTab
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 2.0);
+    CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:108.0/255.0f green:108.0/255.0f blue:108.0/255.0f alpha:1.0].CGColor);
+    CGContextSetFillColorWithColor(context, [UIColor colorWithRed:108.0/255.0f green:108.0/255.0f blue:108.0/255.0f alpha:1.0].CGColor);
+    CGContextSetShadow(context, CGSizeMake (0, 2), 5.0);
+    
+    CGContextSaveGState(context);
+    
+    CGRect rrect = self.bounds;
+    
+    CGFloat radius = 5.0;
+    CGFloat width = CGRectGetWidth(rrect)-5;
+    CGFloat height = CGRectGetHeight(rrect)-4;
+
+    CGContextMoveToPoint(context, 5, 0);
+    CGContextAddArcToPoint(context, 10, 0, 10, 5, radius);
+    CGContextAddLineToPoint(context, 10, height-5);
+    CGContextAddArcToPoint(context, 10,  height, 15,  height, radius);
+    CGContextAddLineToPoint(context, width-10, height);
+    CGContextAddArcToPoint(context, width-5,  height, width-5,  height-5, radius);
+    CGContextAddLineToPoint(context, width-5, radius);
+   CGContextAddArcToPoint(context, width-radius, 0, width, 0, radius);
+  
+   
+    CGContextAddLineToPoint(context, 0, 0);
+    CGContextClosePath(context);
+    CGContextDrawPath(context, kCGPathFill);
+    
+    CGContextRestoreGState(context);
+}
+
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
--- CustomTabBarController/ViewController.m
+++ CustomTabBarController/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-
 @interface ViewController ()
 
 @end
@@ -18,6 +17,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    SlidingTabsControl* tabs = [[SlidingTabsControl alloc] initWithTabCount:3 delegate:self];
+    [self.view addSubview:tabs];
 }
 
 - (void)viewDidUnload
@@ -30,5 +31,27 @@
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
+#pragma mark -
+
+#pragma mark SlidingTabsControl Delegate
+- (UILabel*) labelFor:(SlidingTabsControl*)slidingTabsControl atIndex:(NSUInteger)tabIndex;
+{
+    UILabel* label = [[[UILabel alloc] init] autorelease];
+    label.text = [NSString stringWithFormat:@"Tab %i", tabIndex+1];
+    
+    return label;
+}
+
+- (void) touchUpInsideTabIndex:(NSUInteger)tabIndex
+{
+    
+}
+
+- (void) touchDownAtTabIndex:(NSUInteger)tabIndex
+{
+    
+}
+
+
 
 @end
