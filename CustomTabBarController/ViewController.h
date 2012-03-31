61e2f02d4097e8f685e66350b6657591689e8b37
xxhp <xiao_hb@qq.com>
2012-03-31 23:26:38 +0800

    Initial Commit

---
+++ CustomTabBarController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomTabBarController
+//
+//  Created by xiao haibo on 12-3-31.
+//  Copyright (c) 2012年 xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CustomTabBarController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomTabBarController
+//
+//  Created by xiao haibo on 12-3-31.
+//  Copyright (c) 2012年 xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
